507e9bb715f6696cd8a6272a568ba90433754b5e
Mohammad Asad <dustyaxad4@gmail.com>
2021-02-06 20:13:04 +0500

    all files uploaded

---
+++ server.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req,res) {
+
+    var result=Number(req.body.num1)+Number(req.body.num2);
+
+    res.send("The answer is "+result);
+});
+
+app.listen(3000,function() {
+    console.log("listening at port 3000");
+});
